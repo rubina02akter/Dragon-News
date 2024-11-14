1df72d65518a1d9482d11536c86180a8070e09b2
rubina02akter <rubinaakter0108@gmail.com>
2024-11-14 15:10:47 +0600

    categories btn added dynamically

--- src/components/layout-components/LeftSidebar.jsx
+++ src/components/layout-components/LeftSidebar.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 
 
 const LeftSidebar = () => {
+  const [categories,setCategories] = useState([])
+  useEffect(()=>{
+    fetch("https://openapi.programming-hero.com/api/news/categories")
+    .then(res=>res.json())
+    .then(data=>setCategories(data.data.news_category))
+  },[])
   return (
     <div>
-     <h2 className="font-semibold">All Category</h2>
+     <h2 className="font-semibold">All Category({categories.length})</h2>
+     <div className="flex flex-col gap-2 mt-6">
+      {
+        categories.map((category)=>(
+          <button className="btn" key={category.category_id}>{category.category_name}</button>
+        ))
+      }
+     </div>
     </div>
   );
 };
